f98a3cde71df9321c19bd419bf0cd844649aec46
MaryutinVlad <93710634+MaryutinVlad@users.noreply.github.com>
2022-03-15 20:22:27 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/scripts/index.js'
+        app: './src/pages/index.js'
     },
     output: {
         filename: 'bundle.js',
